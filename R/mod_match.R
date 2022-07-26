e3807a674da5153cc193dbcdd40ca9000af07015
wehsLai <akiya1203@gmail.com>
2022-07-26 19:11:39 +0800

    feat: Add Matches table

---
+++ R/mod_match.R
@@ -0,0 +1,97 @@
+matchUI <- function(id) {
+  ns <- NS(id)
+  reactableOutput(ns("table"))
+}
+
+matchServer <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    output$table <- renderReactable({
+      fmt <- paste0("%0", nchar(as.character(max(rv$ds$matches$noInTournament))), "s")
+
+      data <- rv$ds$matches %>%
+        fivbvis:::v_remap(col = "status", schema = "Volley Match Status") %>%
+        arrange(dateTimeUtc) %>%
+        mutate(
+          noInTournament = sprintf(fmt, noInTournament),
+          dateTimeClient = format(dateTimeUtc, tz = Sys.timezone()),
+          resultText = sprintf("%s %s", matchResultText, setsResultsText)
+        ) %>%
+        select(noInTournament, dateTimeClient, countryName, city, poolName, teamACode, teamBCode, resultText, status)
+
+      teams <- sort(unique(c(data$teamACode, data$teamBCode)))
+
+      match.colDef <- list(
+        noInTournament = colDef(name = "No", minWidth = 55),
+        dateTimeClient = colDef(name = "Date", minWidth = 150),
+        countryName = colDef(
+          name = "Country", minWidth = 150, filterable = TRUE,
+          filterInput = function(values, name) {
+            tags$select(
+              onchange = sprintf("Reactable.setFilter('%s', '%s', event.target.value || undefined)", "table", name),
+              tags$option(value = "", "All"),
+              map(sort(unique(values)), tags$option),
+              "aria-label" = sprintf("Filter %s", name),
+              style = "width: 100%; height: 28px;"
+            )
+          }
+        ),
+        city = colDef(
+          name = "City", minWidth = 180, filterable = TRUE,
+          filterInput = function(values, name) {
+            tags$select(
+              onchange = sprintf("Reactable.setFilter('%s', '%s', event.target.value || undefined)", "table", name),
+              tags$option(value = "", "All"),
+              map(sort(unique(values)), tags$option),
+              "aria-label" = sprintf("Filter %s", name),
+              style = "width: 100%; height: 28px;"
+            )
+          }
+        ),
+        poolName = colDef(
+          name = "Pool", filterable = TRUE,
+          filterInput = function(values, name) {
+            tags$select(
+              onchange = sprintf("Reactable.setFilter('%s', '%s', event.target.value || undefined)", "table", name),
+              tags$option(value = "", "All"),
+              map(sort(unique(values)), tags$option),
+              "aria-label" = sprintf("Filter %s", name),
+              style = "width: 100%; height: 28px;"
+            )
+          }
+        ),
+        teamACode = colDef(
+          name = "Team A", filterable = TRUE,
+          filterInput = function(values, name) {
+            tags$select(
+              onchange = sprintf("Reactable.setFilter('%s', '%s', event.target.value || undefined)", "table", name),
+              tags$option(value = "", "All"),
+              map(teams, tags$option),
+              "aria-label" = sprintf("Filter %s", name),
+              style = "width: 100%; height: 28px;"
+            )
+          },
+          filterMethod = JS("function(rows, columnId, filterValue) {
+                               return rows.filter(function(row) {
+                                                    return (row.values['teamACode'] == filterValue || row.values['teamBCode'] == filterValue)
+                                                  })
+                             }")
+        ),
+        teamBCode = colDef(name = "Team B"),
+        resultText = colDef(name = "Result", minWidth = 250),
+        status = colDef(name = "Status"))
+
+      reactable(
+        data,
+        columns = match.colDef,
+        defaultColDef = df.colDef,
+        style = list(fontFamily = "Source Sans Pro", fontSize = "0.875rem", minWidth = 650),
+        defaultPageSize = 20,
+        resizable = TRUE,
+        highlight = TRUE,
+        compact = TRUE,
+        wrap = TRUE,
+        elementId = "table"
+      )
+    })
+  })
+}
